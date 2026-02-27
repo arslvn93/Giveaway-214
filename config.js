d160c22112a6d38724835915421725c8aabab9aa
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2026-02-27 17:01:12 -0500

    Update Site

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const config = {
   },
   "giveaway": {
     "heroHeadline": "Win <span class='highlight'>Trusted Help</span> for Your Parent’s Move — <span class='highlight'>$1,000 Package</span>!",
-    "heroSubheadline": "Helping a parent downsize or transition can feel overwhelming 🤍\n\n\nThe Peace of Mind Package gives one Halton or Hamilton family guided support across the legal, financial, and mortgage pieces plus a $200 credit for hands-on help from a Senior Move Manager & Life Organizer to help sort belongings and next steps with care. You’ll also receive a $200 Spencer’s at the Waterfront gift card so the coordinating adult children can enjoy a well-deserved night out! \n\nBecause clarity shouldn’t come at the cost of burnout.  Draw April 10, 2026.",
+    "heroSubheadline": "The Peace of Mind Package gives one Halton or Hamilton family guided support across the legal, financial, and mortgage pieces plus a $200 credit for hands-on help from a Senior Move Manager/ Life Organizer to help sort belongings and next steps with care. You’ll also receive a $200 Spencer’s at the Waterfront gift card so the coordinating adult children can enjoy a well-deserved night out! \n\nBecause clarity shouldn’t come at the cost of burnout.  Draw April 10, 2026.",
     "heroBackgroundImageUrl": "https://salesgenius.s3.ca-central-1.amazonaws.com/uploads/upload-ChatGPTImage2026.febr.18.19_24_24.png",
     "promotionDates": "Enter March 1, 2026 to April 9, 2026. Winner drawn April 10, 2026.",
     "endDate": "2026-04-11T02:59:00.000Z",
@@ -182,7 +182,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1772229608021_0",
+      "id": "q_auto_1772229671478_0",
       "questionText": "What's your current situation?",
       "options": [
         {
@@ -204,7 +204,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1772229608021_1",
+      "id": "q_auto_1772229671478_1",
       "questionText": "What are you looking to do?",
       "options": [
         {
@@ -226,7 +226,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1772229608021_2",
+      "id": "q_auto_1772229671478_2",
       "questionText": "When are you planning to move?",
       "options": [
         {
